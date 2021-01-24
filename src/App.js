ecd883c58afc0dc12f99316943b2a1096a616958
isandeepmakwana <isandeepmakwana1@gmail.com>
2021-01-24 07:53:08 +0530

    create app file for join all pages and databases , authtications

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  // dispatch
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+          <div className="app__body">
+            <Router>
+              <Sidebar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  {/* <Chat /> */}
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+
+
+        )
+
+      }
+    </div>
+  );
+}
+
+export default App;
